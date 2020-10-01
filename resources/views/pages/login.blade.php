79ad05abfd65eb915b4144cc6535379641547d4d
Patrick G. Leblanc <p.gleblanc@ti-ssi.com>
2020-10-01 12:28:07 +0000

    remade login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,10 +1,10 @@
 @extends('base_template')
 
 @section('body')
-    <section class="l-vspacing-large">
-        <div class="row justify-content-center">
-	    <div class="large-8 columns">
-			<div class="medium-offset-3 medium-6">
+        <div class="row my-5">
+	    <div class="col-12">
+	    <div class="row">
+			<div class="col-12 offset-lg-4 col-lg-4">
 
     @if ( $errors->any() )
 		    @component('components.alert', ['alertType' => 'danger'])
@@ -24,31 +24,34 @@
 		    @endcomponent
 		@endif
 		           
-</div>		
+</div>
+</div>
 		<form action="{{ url('login') }}" method="POST">
 		    {{ csrf_field() }}
 		    <div class="row">
-			<div class="medium-offset-3 medium-6 columns">
+			<div class="col-12 offset-lg-4 col-lg-4">
 			    <label>Username</label>
 			    <input type="text" class="form-control @if($errors->has('username')) is-invalid @endif" name="username" value="{{ old('username') }}">
 			</div>
             </div>
 		    <div class="row">
-			<div class="medium-offset-3 medium-6 columns">
+			<div class="col-12 col-lg-4 offset-lg-4">
 			    <label>Password</label>
 			    <input type="password" class="form-control @if($errors->has('password')) is-invalid @endif" name="password" value="">
 			</div>
 		    </div>
-		    <div class="d-flex justify-content-center">
-			<input type="submit" class="button primary" value="{{ __('general.login') }}">
+		    <div class="row">
+			<div class="col-12 col-lg-4 offset-lg-4 text-center mt-3">
+			<input type="submit" class="btn btn-primary" value="{{ __('general.login') }}">
 			</div>
-<div class="d-flex justify-content-center">
-			
+			</div>
+		    <div class="row">
+			<div class="col-12 col-lg-4 offset-lg-4 text-center mt-3">
 <a href="{{ url('/register') }}">{{ __('general.register') }}</a>
-		    </div>
+			</div>
+			</div>
 		</form>
 	    </div>
         </div>
-    </section>
 
 @endsection
